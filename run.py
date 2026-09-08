87e6a183938193ed2b739ab40d482874e1055cb6
dasis <dasiscodes@gmail.com>
2026-09-08 19:51:15 +0300

    whatever

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+import traceback
 from app import create_app
 
-app = create_app()
+try:
+    app = create_app()
+except Exception as e:
+    print("CRITICAL BOOT ERROR:")
+    traceback.print_exc()
+    raise e
 
 if __name__ == '__main__':
     app.run()
