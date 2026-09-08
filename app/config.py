bc38eb95c388545c6cff484372f7c874e18e38bc
dasis <dasiscodes@gmail.com>
2026-09-08 16:33:55 +0300

    fixed issue

--- app/config.py
+++ app/config.py
@@ -1,10 +1,15 @@
-import os 
+import os
 
 class Config:
-	SECRET_KEY = os.environ.get('SECRET_KEY')
-	SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-	MAIL_SERVER = 'smtp.gmail.com'
-	MAIL_PORT = 587
-	MAIL_USE_TLS = True
-	MAIL_USERNAME = os.environ.get('EMAIL_USER')
-	MAIL_PASSWORD = os.environ.get('EMAIL_PASS')	
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {
+            "sslmode": "require"
+        }
+    }
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
